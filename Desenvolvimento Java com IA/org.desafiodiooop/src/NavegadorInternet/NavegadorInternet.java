6a8410694bf8da68c4bf8fddfeedb5b5447bc198
Thiago Ripardo <thiago.ripardo.86@gmail.com>
2024-06-24 14:03:04 -0400

    Delete Desenvolvimento Java com IA/org.desafiodiooop directory

--- Desenvolvimento Java com IA/org.desafiodiooop/src/NavegadorInternet/NavegadorInternet.java
+++
@@ -1,7 +0,0 @@
-package NavegadorInternet;
-
-public interface NavegadorInternet {
-    public void exibirPagina(String url);
-    public void adicionarNovaAba();
-    public void atualizarPagina();
-}
